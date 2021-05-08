8da48c3c8d37dadb3d47c29176dcb9573cfd1aa4
Ryan Adi Putra <ryannadiputraa@gmail.com>
2021-05-08 18:13:37 +0800

    init project with Material UI dan redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import "../styles/globals.css";
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: "Montserrat",
+        fontWeightLight: 400,
+        fontWeightRegular: 500,
+        fontWeightMedium: 600,
+        fontWeightBold: 700,
+    },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />;
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
